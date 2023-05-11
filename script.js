823d2cef6f5c0062f84307821d4466a2cb7e7339
alessandralsiqueira <alessandra.lopesiqp@gmail.com>
2023-05-10 21:54:27 -0300

    10/05

---
+++ script.js
@@ -0,0 +1,47 @@
+/*
+        var n1 = window.prompt()
+        var n2 = window.prompt()
+
+        var inteiro1 = parseInt(n1)
+        var inteiro2 = parseInt(n2)
+
+        var soma = inteiro1 + inteiro2
+        var sub = inteiro1 - inteiro2
+        var div = inteiro1 / inteiro2
+        var multi = inteiro1 * inteiro2
+        var resto = inteiro1 % inteiro2
+
+        window.alert('Todas as operações foram realizadas e aqui está o resultado:' + '\n' + 'soma: ' + soma + '\n' + 'subtração: ' + sub + '\n' + 'divisão: ' + div + '\n' + 'multiplicação: ' + multi + '\n' + 'resto: ' + resto)
+
+        var bol = (inteiro1 === inteiro2)
+        var bol1 = (inteiro1 !== inteiro2)
+        var bol2 = (inteiro1 <= inteiro2)
+        var bol3 = (inteiro1 >= inteiro2)
+        var bol4 = (inteiro1 < inteiro2)
+        var bol5 = (inteiro1 > inteiro2)
+
+            window.alert('Todas as operações foram realizadas e aqui está o resultado:'
+         + '\n' + '1 ' + bol + '\n'  + bol1 + '\n'
+         + bol2 + '\n' + bol3 + '\n' + bol4 + '\n' + bol5)
+
+
+        
+         var n1 = window.prompt('Escreva sua cor favorita:')
+
+       if(n1 === 'azul'){
+        alert('A cor escolhida é azul')
+       }
+       else if(n1 === 'vermelho'){
+        alert('A cor escolhida é vermelha') 
+       }
+       else if(n1 === 'verde'){
+        alert('A cor escolhida é verde')
+       }
+       else if(n1 === 'amarela'){
+        alert('A cor escolhida é amarela')
+       }
+       else{
+        alert('A cor não é uma cor primária!')
+       }
+*/ 
+  
